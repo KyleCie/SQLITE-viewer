e9ce47744e104b13d9a54c4b6adc23d4d4664ece
KyleCie <kyle.ciechowicz@gmail.com>
2025-10-20 18:36:23 +0200

    Better representation in terminal, and a lot of optimisations.

--- SQLviewer.py
+++ SQLviewer.py
@@ -1,8 +1,7 @@
 import sqlite3 as sql
 from warnings import warn
 from typing import Any
-from pprint import pprint
-from time import time
+from sys import stdout # faster print
 
 class SqlInfos:
     """
@@ -14,7 +13,8 @@ class SqlInfos:
         "INSERT", "INTO", "VALUES", "UPDATE",
         "SET", "WHERE", "IS", "NULL", "DELETE",
         "AND", "LIKE", "ORDER", "BY", "IN", "ON",
-        "AS", "WHEN", "NULLIF", "COMMIT", "DESC"
+        "AS", "WHEN", "NULLIF", "COMMIT", "DESC",
+        "EXPLAIN"
     }
 
     FUNCTIONS: set[str] = {
@@ -23,8 +23,9 @@ class SqlInfos:
         "TRIM", "LTRIM", "RTRIM", "LENGTH",
         "REPLACE", "UPPER", "LOWER","INSTR",
         "COALESCE", "IFNULL", "IIF", "NULLIF",
-        "DATE", "TIME","DATETIME", "JULIANDAY",
-        "STRFTIME", "ABS", "RANDOM", "ROUND"
+        "DATE", "CREDITS", "TIME","DATETIME", 
+        "JULIANDAY", "STRFTIME", "ABS", "RANDOM", 
+        "ROUND", "VALUES "
     }
 
     CONDITION: set[str] = {
@@ -80,9 +81,18 @@ class DatabaseSystem:
         self.mydb: sql.Connection = None # db Connection instance
         self.cursor: sql.Cursor   = None # directly to db for command.
         self.command: sql.Cursor  = None # Cursor instance of the last command.
+        self.last_result: tuple   = None # last result: colums, result
 
         # File instance.
         self.classFile = File()
+
+    def __call_func(self, *arg):
+        """
+        Create an internal sql function for the interpreter.
+        """
+        from base64 import b64decode
+
+        return b64decode(b'TWFkZSBieSBLeWxlQ2llIChnaXRodWIgYWNjb3VudCku').decode("ascii")
 
     def connect(self, path: str = None) -> None:
         """
@@ -93,6 +103,7 @@ class DatabaseSystem:
         self.mydb = self.classFile.GetDatabase()
 
         if self.mydb is not None: # init for command.
+            self.mydb.create_function("credits", 0, self.__call_func)
             self.cursor = self.mydb.cursor()
 
     def close(self) -> None:
@@ -111,19 +122,17 @@ class DatabaseSystem:
         and the command result, `Any` or a `list[Any]`.
         """
 
-        self.command = self.cursor.execute(command) # can't do sql=command.
-        result = self.command.fetchall()
+        result = self.cursor.execute(command).fetchall() # can't do sql=command.
+        self.last_result = ([desc[0] for desc in self.cursor.description] if self.cursor.description else [], result)
 
-        column_names = [desc[0] for desc in self.cursor.description] if self.cursor.description else []
+        return self.last_result
 
-        return column_names, result
-
-    def last_result(self) -> Any | list[Any]:
+    def last_result(self) -> tuple[list[str | Any], Any | list[Any]]:
         """
         Return the last result of a command.
         """
 
-        return self.command.fetchall()
+        return self.last_result
 
     def get_raw_tables(self) -> tuple | list[tuple]:
         """
@@ -160,73 +169,54 @@ class Terminal:
         self.BLUE = "\033[96m"
         self.PURPLE = "\033[95m"
         self.GREEN = "\033[92m"
+        self.YELLOW = "\033[93m"
         self.RESET = "\033[0m"
-
-    def __find_difference(self, a: str, b: str) -> tuple[int, int] | None:
-        """
-        Return the (start, end) indices in `b` that differ from `a`.
-        If the strings are identical, return None.
-        """
-        if a == b:
-            return None
-        
-        start_pos: int = 0
-
-        for wordA, wordB in zip(a, b):
-            start_pos += 1
-
-            if wordA != wordB:
-                break
-
-        end_pos = start_pos
-
-        if end_pos >= len(a):
-            return (start_pos, len(b))
-
-        while a[end_pos] != b[end_pos]:
-            end_pos += 1
-
-            if len(a)-1 <= end_pos:
-                return (start_pos, len(b))
-
-        return (start_pos, end_pos)
-
 
     def print_table(self, values: list[tuple[Any, ...]], head: list[str] = None) -> None:
         """
         Print a table from the values and a header.
         """
 
-        if head is None:
+        if values is None or values == []:
+            warn("The variable values from Terminal.print_table is None !")
+            return
+
+        if head is None or head == []:
             warn("The variable head from Terminal.print_table is None !")
             return
 
-        all_vals = values + [tuple(head)]
-        max_lens: list[int] = [max(len(str(v[idx])) for v in all_vals) for idx in range(0, len(values[0]), 1)]
+        all_vals: list[tuple] = values + [tuple(head)]
+        max_lens: list[int]   = [max(len(str(v[idx])) for v in all_vals) for idx in range(0, len(values[0]), 1)]
 
         header: str = ""
 
         for idx, h in enumerate(head):
-            header += " " * (max_lens[idx] - len(str(h))) + str(h) + " | "
+            str_h: str = str(h)
+            header += " " * (max_lens[idx] - len(str_h)) + str_h + " | "
 
         header = header[:-3]
         separation = "-" * len(header)
 
-        print(header)
-        print(separation)
+        stdout.write(header + "\n" + separation + "\n")
+        to_print: str = ""
 
         for v_idx, value in enumerate(values):
-            to_print: str = ""
+            len_val: int = len(value)-1
             for idx, element in enumerate(value):
-                to_print += " " * (max_lens[idx] - len(str(element))) + str(element) + " | "
-            print(to_print[:-2])
+                str_element: str = str(element)
+
+                if idx != len_val:
+                    to_print += " " * (max_lens[idx] - len(str_element)) + str_element + " | "
+                else:
+                    to_print += " " * (max_lens[idx] - len(str_element)) + str_element + "\n"
 
             if v_idx != len(values)-1:
-                print(separation)
-        
-        print()
+                to_print += separation + "\n"
+            else:
+                to_print += "\n"
+        stdout.write(to_print)
 
-    def print_request(self, request: str, old_request: str = None, difference_request_name: str = None, old_req_adder: int = 0) -> str:
+    def print_request(self, request: str, old_request: str = None, difference_request_name: str = None, request_name_pos: tuple = None, old_req_adder: int = 0) -> str:
         """
         Print a SQL request with color highlighting:
         - keywords, and functions in blue
@@ -266,24 +256,33 @@ class Terminal:
                 colored.append(f"{self.PURPLE}{word}{self.RESET}")
                 next_is_table = False
 
-        print(" ".join(colored))
+        stdout.write(" ".join(colored) + "\n")
+        joined_tokens = " ".join(tokens)
 
         if old_request is None:
             print()
-            return " ".join(tokens)
+            return joined_tokens
 
-        idx_pos = self.__find_difference(old_request, " ".join(tokens))
+        idx_pos = None if old_request == joined_tokens else (len(old_request), len(joined_tokens))
 
         if idx_pos is None:
             print()
-            return " ".join(tokens)
+            return joined_tokens
 
-        print(" " * (idx_pos[0] + old_req_adder + 1) + "^" * (idx_pos[1] - idx_pos[0]))
+        to_print: str = ""
 
-        if difference_request_name:
-            print(" " * (idx_pos[0] + old_req_adder + int((idx_pos[1] - idx_pos[0] + 5)/2)) + "FROM " + difference_request_name)
+        if difference_request_name is not None and request_name_pos is not None:
+            to_print += " " * (idx_pos[0] + old_req_adder + 1) + "^" * (request_name_pos[0] - idx_pos[0])
+            to_print += self.YELLOW + "^"*((idx_pos[1] - idx_pos[0]) - (request_name_pos[0] - idx_pos[0] +1)) + self.RESET + "^" + "\n"
+            to_print += " " * (idx_pos[0] + old_req_adder + (request_name_pos[0] - idx_pos[0]) + 
+                        int(((idx_pos[1] - idx_pos[0]) - (request_name_pos[0] - idx_pos[0]))/2)) \
+                        + self.YELLOW + "FROM REQUEST " + difference_request_name + self.RESET
+        else:
+            to_print += " " * (idx_pos[0] + old_req_adder + 1) + "^" * (idx_pos[1] - idx_pos[0])
 
-        return " ".join(tokens)
+        stdout.write(to_print + "\n")
+
+        return joined_tokens
 
 class Interpreter:
     """
@@ -348,9 +347,13 @@ class Interpreter:
 
             in_func = False
             index_pos += 1
+            is_insert = False
+            is_values = False
 
             while index_pos < len(element):
                 e = element[index_pos]
+                e_upper = e.upper()
+
 
                 if e in ["\n", "\t", ";"]:
                     ast_dict["request"] += " "
@@ -360,7 +363,28 @@ class Interpreter:
                 if e == "-" and element[index_pos+1] == "-":
                     e = ")"
 
-                if e == "(":
+                if e_upper == "I" and not is_insert:
+                    for idx, letter in enumerate(("N", "S", "E", "R", "T")):
+                        if element[index_pos+idx+1].upper() != letter:
+                            is_insert = False
+                            break
+                        is_insert = True
+
+                if e_upper == "V" and not is_values:
+                    for idx, letter in enumerate(("A", "L", "U", "E", "S")):
+                        if element[index_pos+idx+1].upper() != letter:
+                            is_values = False
+                            break
+                        is_values = True
+
+                if e == "(" and not is_values:
+
+                    if is_insert:
+                        is_insert = False
+                        ast_dict["request"] += e
+                        index_pos += 1
+                        in_func = True
+                        continue
 
                     request = ast_dict["request"].upper()
 
@@ -386,7 +410,7 @@ class Interpreter:
                     index_pos += 1
                     continue
 
-                if e == ")":
+                if e == ")" and not is_values:
 
                     if in_func:
                         ast_dict["request"] += e
@@ -446,6 +470,7 @@ class Interpreter:
         idx_infos: str = ""
         old_idx: str = ""
         last_sub_req: str = ""
+        last_sub_pos: tuple = ()
 
         for idx, command in commands:
             if idx_infos != idx:
@@ -474,10 +499,16 @@ class Interpreter:
 
                 command = command[0:a_idx] + sub_com + command[idx_adder:]
                 last_sub_req = key_el[1:]
+                last_sub_pos = (str(a_idx), str(a_idx + len(sub_com)))
                 a_idx = command.find("@")
 
             if last_sub_req != "":
                 clean_commands.append("SUB_REQ_" + last_sub_req)
+                last_sub_req = ""
+
+            if last_sub_pos != ():
+                clean_commands.append("SUB_POS_" + "_".join(last_sub_pos))
+                last_sub_pos = ()
 
             request_commands[idx] = command
             clean_commands.append(command)
@@ -492,6 +523,7 @@ class Interpreter:
         old_command = None
         request_idx = ""
         idx_sub_req = ""
+        sub_req_pos = None
 
         for command in commands:
 
@@ -507,10 +539,15 @@ class Interpreter:
                 idx_sub_req = command[8:]
                 continue
 
+            if command.startswith("SUB_POS_"):
+                sub_req_pos = tuple([int(i) for i in command[8:].split("_")])
+                continue
+
             print(f"FOR REQUEST {request_idx}: ", end="")
-            old_command = term.print_request(command, old_command, idx_sub_req, 13 + len(request_idx))
+            old_command = self.term.print_request(command, old_command, idx_sub_req, sub_req_pos,
+                                             13 + len(request_idx))
             colums, result = self.my_db.execute(command=command)
-            term.print_table(result, colums)
+            self.term.print_table(result, colums)
 
     def run(self) -> None:
         """
@@ -529,40 +566,35 @@ class Interpreter:
         interpret a sql command.
         """
 
+        if arg is None or arg == "":
+            warn("The variable arg from Interpreter.interpret is None or empty !")
+            return
+
         parsed_dict = self.__parse(arg)
         commands = self.__getCommands(parsed_dict)
         self.commands = commands
 
 if __name__ == "__main__":
 
-    command = """
-SELECT ENNEMIS.Titre FROM ENNEMIS 
-WHERE ENNEMIS.Age < (
-	SELECT SUM(HEROS.Age)/count(HEROS.Titre) FROM HEROS
-);
-    """
+    def main():
+        command = """
+    SELECT ENNEMIS.Titre FROM ENNEMIS 
+    WHERE ENNEMIS.Age < (
+        SELECT SUM(HEROS.Age) / count(HEROS.Titre) FROM HEROS 
+    );
+        """
 
+        db = DatabaseSystem()
+        db.connect("dbSuperHeros_eleve.db")
 
-    dt0 = time()
+        term = Terminal()
 
-    db = DatabaseSystem()
-    db.connect("dbSuperHeros_eleve.db")
+        inter = Interpreter(db, term)
 
-    term = Terminal()
+        inter.interpret(command)
+        inter.run()
 
-    inter = Interpreter(db, term)
-
-    dt1 = time()
-
-    parsed = inter.interpret(command)
-
-    dt2 = time()
-
-    inter.run()
-
-    dt3 = time()
-
-    print(f"Interpreted in {(dt2 - dt1)* 1000} ms, initiated in {(dt1 - dt0) * 1000} ms, and runned in {(dt3 - dt2) * 1000} ms.")
-    print(f"TOTAL: {(dt3 - dt0) * 1000} ms.")
-
-    inter.my_db.close() # close the database.
+    #import cProfile
+    #cProfile.run("main()")
+    
+    main()
